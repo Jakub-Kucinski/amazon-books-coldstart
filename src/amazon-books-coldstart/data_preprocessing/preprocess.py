e45893054cf645df3961fda6cfd5fe989e4df88e
Karol Kuczmarz <kj.kuczmarz@gmail.com>
2023-06-27 00:01:06 +0200

    Embeddings creation * code clean up with pre-commit * added initial model download * updated pyproject.toml

--- src/amazon-books-coldstart/data_preprocessing/preprocess.py
+++ src/amazon-books-coldstart/data_preprocessing/preprocess.py
@@ -1,5 +1,7 @@
 import csv
+
 import numpy as np
+
 
 class Book:
     def __init__(self, row, id):
@@ -14,15 +16,17 @@ class Book:
         self.infoLink = str(row[7])
         self.categories = row[8]
         self.ratingsCount = row[9]
-        
+
+
 class Review:
     def __init__(self, row):
         def parse_helpfulness(helpfulness):
-            helpfulness = helpfulness.split('/')
+            helpfulness = helpfulness.split("/")
             return [int(helpfulness[0]), int(helpfulness[1])]
+
         self.book_id = str(row[0])
         self.book_title = str(row[1])
-        if row[2] == '':
+        if row[2] == "":
             self.price = 0
         else:
             self.price = float(row[2])
@@ -34,21 +38,25 @@ class Review:
         self.summary = str(row[8])
         self.text = str(row[9])
 
-class Data_reader():
+
+class Data_reader:
     def __init__(self):
         self.title_to_id = {}
         self.ratings = {}
         self.books = {}
         self.read_ratings()
         self.read_books()
-        # 0.7 - train, 0.2 - validation, 0.1 - test        
+        # 0.7 - train, 0.2 - validation, 0.1 - test
         self.split_dataset(0.7, 0.2)
 
     def read_ratings(self):
         i = 0
         success = 0
-        with open('../../../data/01_raw/Books_rating.csv', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        with open(
+            "../../../data/01_raw/Books_rating.csv",
+            newline="",
+        ) as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=",", quotechar="|")
             for row in spamreader:
                 if i > 0:
                     try:
@@ -56,7 +64,7 @@ class Data_reader():
                         self.ratings[str(row[0])] = review
                         self.title_to_id[review.book_title] = review.book_id
                         success += 1
-                    except:
+                    except:  # noqa
                         pass
                 i += 1
         print("Read " + str(success) + " out of " + str(i) + " ratings.")
@@ -64,15 +72,18 @@ class Data_reader():
     def read_books(self):
         i = 0
         success = 0
-        with open('../../../data/01_raw/books_data.csv', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        with open(
+            "../../../data/01_raw/books_data.csv",
+            newline="",
+        ) as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=",", quotechar="|")
             for row in spamreader:
                 if i > 0:
                     try:
                         id = self.title_to_id[row[0]]
                         self.books[id] = Book(row, id)
                         success += 1
-                    except:
+                    except:  # noqa
                         pass
                 i += 1
         print("Read " + str(success) + " out of " + str(i) + " books.")
@@ -80,29 +91,32 @@ class Data_reader():
     def split_dataset(self, train_size, valid_size):
         train_size = int(len(self.title_to_id) * train_size)
         valid_size = int(len(self.title_to_id) * valid_size)
-        test_size = len(self.title_to_id) - train_size - valid_size
         np.random.seed(0)
-        all_indices = np.random.choice(list(self.title_to_id.values()), size=len(self.title_to_id), replace=False)
+        all_indices = np.random.choice(
+            list(self.title_to_id.values()),
+            size=len(self.title_to_id),
+            replace=False,
+        )
         train_indices = set(all_indices[:train_size])
-        valid_indices = set(all_indices[train_size: train_size + valid_size])
-        test_indices = set(all_indices[train_size + valid_size: ])
-        ratings = {'test': {}, 'train' : {}, 'validation' : {}}
-        books = {'test': {}, 'train' : {}, 'validation' : {}}
+        valid_indices = set(all_indices[train_size : train_size + valid_size])
+        test_indices = set(all_indices[train_size + valid_size :])
+        ratings = {"test": {}, "train": {}, "validation": {}}
+        books = {"test": {}, "train": {}, "validation": {}}
         for id in self.ratings.keys():
             review = self.ratings[id]
             if review.book_id in train_indices:
-                ratings['train'][id] = review
+                ratings["train"][id] = review
             if review.book_id in valid_indices:
-                ratings['validation'][id] = review
+                ratings["validation"][id] = review
             if review.book_id in test_indices:
-                ratings['test'][id] = review
+                ratings["test"][id] = review
         for id in self.books.keys():
             book = self.books[id]
             if book.id in train_indices:
-                books['train'][id] = review
+                books["train"][id] = review
             if book.id in valid_indices:
-                books['validation'][id] = review
+                books["validation"][id] = review
             if book.id in test_indices:
-                books['test'][id] = review
+                books["test"][id] = review
         self.ratings = ratings
         self.books = books
---
+++ src/amazon-books-coldstart/embeddings_creation/text_embeddings.py
@@ -0,0 +1,5 @@
+from sentence_transformers import SentenceTransformer
+
+MODEL_NAME = "all-mpnet-base-v2"
+
+model = SentenceTransformer(MODEL_NAME)
