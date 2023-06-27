991af0b61b7467fc97fcf1278dfaac17cab291cc
Jakub-Kucinski <jakub.kucinski21@gmail.com>
2023-06-27 17:44:06 +0200

    Added pandas preprocessing script

--- src/amazon-books-coldstart/data-preprocessing/data_reader.py
+++ src/amazon-books-coldstart/data-preprocessing/data_reader.py
@@ -1,11 +1,13 @@
 import csv
+
 import numpy as np
+
 
 class Book:
     def __init__(self, row, id):
         self.id = id
         self.title = str(row[0])
-        self.desciption = str(row[1])
+        self.description = str(row[1])
         self.authors = row[2]
         self.image = str(row[3])
         self.previewLink = str(row[4])
@@ -14,15 +16,17 @@ class Book:
         self.infoLink = str(row[7])
         self.categories = row[8]
         self.ratingsCount = row[9]
-        
+
+
 class Review:
     def __init__(self, row):
         def parse_helpfulness(helpfulness):
-            helpfulness = helpfulness.split('/')
+            helpfulness = helpfulness.split("/")
             return [int(helpfulness[0]), int(helpfulness[1])]
+
         self.book_id = str(row[0])
         self.book_title = str(row[1])
-        if row[2] == '':
+        if row[2] == "":
             self.price = 0
         else:
             self.price = float(row[2])
@@ -34,29 +38,32 @@ class Review:
         self.summary = str(row[8])
         self.text = str(row[9])
 
-class Data_reader():
+
+class Data_reader:
     def __init__(self):
         self.title_to_id = {}
         self.ratings = {}
         self.books = {}
         self.read_ratings()
         self.read_books()
-        # 0.7 - train, 0.2 - validation, 0.1 - test        
+        # 0.7 - train, 0.2 - validation, 0.1 - test
         self.split_dataset(0.7, 0.2)
 
     def read_ratings(self):
         i = 0
         success = 0
-        with open('../../data/01_raw/Books_rating.csv', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in spamreader:
+        with open("../../data/01_raw/Books_rating.csv", newline="") as csv_file:
+            spam_reader = csv.reader(csv_file, delimiter=",", quotechar="|")
+            for row in spam_reader:
                 if i > 0:
                     try:
                         review = Review(row)
-                        self.ratings[str(row[0])] = review
+                        if str(row[0]) not in self.ratings.keys():
+                            self.ratings[str(row[0])] = []
+                        self.ratings[str(row[0])].append(review)
                         self.title_to_id[review.book_title] = review.book_id
                         success += 1
-                    except:
+                    except:  # noqa
                         pass
                 i += 1
         print("Read " + str(success) + " out of " + str(i) + " ratings.")
@@ -64,15 +71,15 @@ class Data_reader():
     def read_books(self):
         i = 0
         success = 0
-        with open('../../data/01_raw/books_data.csv', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in spamreader:
+        with open("../../data/01_raw/books_data.csv", newline="") as csv_file:
+            spam_reader = csv.reader(csv_file, delimiter=",", quotechar="|")
+            for row in spam_reader:
                 if i > 0:
                     try:
                         id = self.title_to_id[row[0]]
                         self.books[id] = Book(row, id)
                         success += 1
-                    except:
+                    except:  # noqa
                         pass
                 i += 1
         print("Read " + str(success) + " out of " + str(i) + " books.")
@@ -80,29 +87,31 @@ class Data_reader():
     def split_dataset(self, train_size, valid_size):
         train_size = int(len(self.title_to_id) * train_size)
         valid_size = int(len(self.title_to_id) * valid_size)
-        test_size = len(self.title_to_id) - train_size - valid_size
+        # test_size = len(self.title_to_id) - train_size - valid_size
         np.random.seed(0)
-        all_indices = np.random.choice(list(self.title_to_id.values()), size=len(self.title_to_id), replace=False)
+        all_indices = np.random.choice(
+            list(self.title_to_id.values()), size=len(self.title_to_id), replace=False
+        )
         train_indices = set(all_indices[:train_size])
-        valid_indices = set(all_indices[train_size: train_size + valid_size])
-        test_indices = set(all_indices[train_size + valid_size: ])
-        ratings = {'test': {}, 'train' : {}, 'validation' : {}}
-        books = {'test': {}, 'train' : {}, 'validation' : {}}
+        valid_indices = set(all_indices[train_size : train_size + valid_size])
+        test_indices = set(all_indices[train_size + valid_size :])
+        ratings = {"test": {}, "train": {}, "validation": {}}
+        books = {"test": {}, "train": {}, "validation": {}}
         for id in self.ratings.keys():
             review = self.ratings[id]
             if review.book_id in train_indices:
-                ratings['train'][id] = review
+                ratings["train"][id] = review
             if review.book_id in valid_indices:
-                ratings['validation'][id] = review
+                ratings["validation"][id] = review
             if review.book_id in test_indices:
-                ratings['test'][id] = review
+                ratings["test"][id] = review
         for id in self.books.keys():
             book = self.books[id]
             if book.id in train_indices:
-                books['train'][id] = book
+                books["train"][id] = book
             if book.id in valid_indices:
-                books['validation'][id] = book
+                books["validation"][id] = book
             if book.id in test_indices:
-                books['test'][id] = book
+                books["test"][id] = book
         self.ratings = ratings
         self.books = books
---
+++ src/amazon-books-coldstart/data-preprocessing/pandas_preprocessing.py
@@ -0,0 +1,158 @@
+from pathlib import Path
+
+import click
+import pandas as pd
+
+
+def read_books(path="data/01_raw/books_data.csv"):
+    books = pd.read_csv(path)
+    return books
+
+
+def read_ratings(path="data/01_raw/Books_rating.csv"):
+    ratings = pd.read_csv(path)
+    return ratings
+
+
+def filter_data(
+    ratings,
+    books,
+    remove_no_description=True,
+    remove_no_publisher=True,
+    remove_no_author=True,
+    remove_no_title=True,
+    remove_no_category=True,
+):
+    if remove_no_description:
+        books = books[books["description"].notna()]
+    if remove_no_publisher:
+        books = books[books["publisher"].notna()]
+    if remove_no_author:
+        books = books[books["authors"].notna()]
+    if remove_no_title:
+        books = books[books["Title"].notna()]
+    if remove_no_category:
+        books = books[books["categories"].notna()]
+    books = books.join(
+        ratings[["Id", "Title"]].drop_duplicates().set_index("Title"),
+        on="Title",
+        how="inner",
+    )
+
+    books = books[books["Title"].isin(ratings["Title"])]
+    ratings = ratings[ratings["Title"].isin(books["Title"])]
+    books = books[books["Id"].isin(ratings["Id"])]
+    ratings = ratings[ratings["Id"].isin(books["Id"])]
+    return ratings, books
+
+
+def split_data_by_books(ratings, books, train=0.7, validation=0.2, test=0.1):
+    if abs(1 - train + validation + test) < 1e-5:
+        raise ValueError("Train, validation and test sizes must sum to 1")
+    ratings = ratings[["Id", "User_id", "review/score"]]
+    ratings = ratings.rename(
+        columns={"Id": "book_id", "User_id": "user_id", "review/score": "score"}
+    )
+    books = books[
+        ["Id", "Title", "authors", "description", "publisher", "categories", "image"]
+    ]
+    books = books.rename(
+        columns={
+            "Id": "book_id",
+            "Title": "title",
+            "authors": "authors",
+            "description": "description",
+            "publisher": "publisher",
+            "categories": "categories",
+            "image": "image",
+        }
+    )
+    books = books.sample(frac=1, random_state=42)
+    train_size = int(len(books) * train)
+    validation_size = int(len(books) * validation)
+    train_books = books[:train_size]
+    validation_books = books[train_size : train_size + validation_size]
+    test_books = books[train_size + validation_size :]
+    train_ratings = ratings[ratings["book_id"].isin(train_books["book_id"])]
+    validation_ratings = ratings[ratings["book_id"].isin(validation_books["book_id"])]
+    test_ratings = ratings[ratings["book_id"].isin(test_books["book_id"])]
+    return (
+        train_ratings,
+        validation_ratings,
+        test_ratings,
+        train_books,
+        validation_books,
+        test_books,
+    )
+
+
+@click.command()
+@click.option("--data_path", default="data/01_raw/")
+@click.option("--destination_path", default="data/02_intermediate/")
+@click.option("--ratings_file", default="Books_rating.csv")
+@click.option("--books_file", default="books_data.csv")
+@click.option("--dont_remove_description", is_flag=True, default=False)
+@click.option("--dont_remove_publisher", is_flag=True, default=False)
+@click.option("--dont_remove_author", is_flag=True, default=False)
+@click.option("--dont_remove_title", is_flag=True, default=False)
+@click.option("--dont_remove_category", is_flag=True, default=False)
+@click.option("--train", default=0.7)
+@click.option("--validation", default=0.2)
+@click.option("--test", default=0.1)
+@click.option("--verbose", is_flag=True, default=False)
+def main(
+    data_path,
+    destination_path,
+    ratings_file,
+    books_file,
+    dont_remove_description,
+    dont_remove_publisher,
+    dont_remove_author,
+    dont_remove_title,
+    dont_remove_category,
+    train,
+    validation,
+    test,
+    verbose,
+):
+    data_path = Path(data_path)
+    if verbose:
+        print("Reading data")
+    ratings = read_ratings(data_path / ratings_file)
+    books = read_books(data_path / books_file)
+    if verbose:
+        print("Filtering data")
+    ratings, books = filter_data(
+        ratings,
+        books,
+        remove_no_description=not dont_remove_description,
+        remove_no_publisher=not dont_remove_publisher,
+        remove_no_author=not dont_remove_author,
+        remove_no_title=not dont_remove_title,
+        remove_no_category=not dont_remove_category,
+    )
+    if verbose:
+        print("Splitting data")
+    (
+        train_ratings,
+        validation_ratings,
+        test_ratings,
+        train_books,
+        validation_books,
+        test_books,
+    ) = split_data_by_books(
+        ratings, books, train=train, validation=validation, test=test
+    )
+    if verbose:
+        print("Saving data")
+    destination_path = Path(destination_path)
+    train_ratings.to_csv(destination_path / "train_ratings.csv", index=False)
+    validation_ratings.to_csv(destination_path / "validation_ratings.csv", index=False)
+    test_ratings.to_csv(destination_path / "test_ratings.csv", index=False)
+    train_books.to_csv(destination_path / "train_books.csv", index=False)
+    validation_books.to_csv(destination_path / "validation_books.csv", index=False)
+    test_books.to_csv(destination_path / "test_books.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
