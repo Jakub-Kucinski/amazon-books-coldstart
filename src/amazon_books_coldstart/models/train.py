585e4eda79e3dccf0706b861228746aab12f1078
Jakub-Kucinski <jakub.kucinski21@gmail.com>
2023-06-29 21:27:59 +0200

    Add model saving

--- src/amazon_books_coldstart/models/train.py
+++ src/amazon_books_coldstart/models/train.py
@@ -12,7 +12,15 @@ from src.amazon_books_coldstart.config import OUTPUT_SIZE
 from src.amazon_books_coldstart.models.dataset import get_train_dataloader
 
 
-def train_single_epoch(model, loss_func, train_loader, optimizer, epoch):
+def train_single_epoch(
+    model,
+    loss_func,
+    train_loader,
+    optimizer,
+    epoch,
+    log_interval=100,
+    save_checkpoint_every=1000,
+):
     model.train()
     for batch_idx, (data, data2) in tqdm(
         enumerate(train_loader), total=len(train_loader), leave=False
@@ -23,8 +31,10 @@ def train_single_epoch(model, loss_func, train_loader, optimizer, epoch):
         loss = loss_func(embeddings, embeddings2)
         loss.backward()
         optimizer.step()
-        if batch_idx % 100 == 0:
+        if batch_idx % log_interval == 0:
             print("Epoch {} Iteration {}: Loss = {}".format(epoch, batch_idx, loss))
+        if batch_idx % save_checkpoint_every == 0:
+            save_model(model, f"data/06_models/{epoch}_{batch_idx}.pth")
 
 
 def wrap_loss_function(loss_function):
@@ -39,6 +49,7 @@ def start_training(
     model_params=dict(),
     optimizer_params=dict(),
     num_epochs=5,
+    save_checkpoint_every=1000,
 ):
     loss_func = wrap_loss_function(loss)
 
@@ -48,7 +59,23 @@ def start_training(
     optimizer = optim.Adam(model.parameters(), **optimizer_params)
 
     for epoch in tqdm(range(1, num_epochs + 1)):
-        train_single_epoch(model, loss_func, train_loader, optimizer, epoch)
+        train_single_epoch(
+            model,
+            loss_func,
+            train_loader,
+            optimizer,
+            epoch,
+            save_checkpoint_every=save_checkpoint_every,
+        )
+
+
+def save_model(model, file_path):
+    torch.save(model, file_path)
+
+
+def load_model(file_path):
+    model = torch.load(file_path)
+    return model
 
 
 torch.manual_seed(2137)
@@ -65,7 +92,7 @@ model_params = {
     "linear1_size": 1024,
     "linear2_size": 256,
 }
-optimizer_params = {"lr": 0.001}
+optimizer_params = {"lr": 0.0001}
 df_books = pd.read_csv("data/02_intermediate/train_books.csv")
 df_ratings = pd.read_csv("data/02_intermediate/train_ratings.csv")
 embeddings = np.load("data/03_primary/train_embeddings.npy")
@@ -73,4 +100,12 @@ book_2_row = json.load(open("data/03_primary/train_id_2_row.json", mode="r"))
 train_loader = get_train_dataloader(
     df_books, df_ratings, embeddings, book_2_row, device, batch_size=512, shuffle=True
 )
-start_training("AmazonBooksModelv3", loss, train_loader, model_params, optimizer_params)
+start_training(
+    "AmazonBooksModelv3",
+    loss,
+    train_loader,
+    model_params,
+    optimizer_params,
+    num_epochs=5,
+    save_checkpoint_every=200,
+)
