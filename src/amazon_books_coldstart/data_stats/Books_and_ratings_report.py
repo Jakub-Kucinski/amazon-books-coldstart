2ea14acc49da275798cdeac1984e5f81696df04b
Rzepa <mateuszrzepecki.99@gmail.com>
2023-06-29 19:40:37 +0200

    Adding report generation and the reports

---
+++ src/amazon_books_coldstart/data_stats/Books_and_ratings_report.py
@@ -0,0 +1,11 @@
+from ydata_profiling import ProfileReport
+import pandas as pd
+
+books = pd.read_csv("data/02_intermediate/train_books.csv")
+ratings = pd.read_csv("data/02_intermediate/train_ratings.csv")
+
+profile = ProfileReport(books, title="Books' profiling report")
+profile.to_file("data/reports/Books_report.html")
+
+profile = ProfileReport(ratings, title="Ratings' profiling report")
+profile.to_file("data/reports/Ratings_report.html")
