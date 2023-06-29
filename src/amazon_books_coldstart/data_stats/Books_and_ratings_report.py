ad8bf6b0d3ea5fb70dd70cada49af8c1cc8649bd
Rzepa <mateuszrzepecki.99@gmail.com>
2023-06-30 01:08:37 +0200

    Changing report location

--- src/amazon_books_coldstart/data_stats/Books_and_ratings_report.py
+++ src/amazon_books_coldstart/data_stats/Books_and_ratings_report.py
@@ -5,7 +5,7 @@ books = pd.read_csv("data/02_intermediate/train_books.csv")
 ratings = pd.read_csv("data/02_intermediate/train_ratings.csv")
 
 profile = ProfileReport(books, title="Books' profiling report")
-profile.to_file("data/reports/Books_report.html")
+profile.to_file("data/08_reporting/Books_report.html")
 
 profile = ProfileReport(ratings, title="Ratings' profiling report")
-profile.to_file("data/reports/Ratings_report.html")
+profile.to_file("data/08_reporting/Ratings_report.html")
