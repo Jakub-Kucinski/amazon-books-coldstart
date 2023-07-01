d33f6aa1f441e133751b4cac9c7caed79b188354
Rzepa <mateuszrzepecki.99@gmail.com>
2023-07-01 02:50:33 +0200

    Adding Index class which creates index from embeddings and Index_cosine_similarity which uses cosine similarity

--- src/amazon_books_coldstart/models/booksindex.py
+++ src/amazon_books_coldstart/models/booksindex.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 import faiss
 from sentence_transformers import SentenceTransformer
@@ -21,3 +22,39 @@ class BooksIndex:
         return [distance for distance in distances.reshape((-1))], [
             self.row_2_book[row] for row in rows.reshape((-1))
         ]
+
+class Index:
+    def __init__(self, embeddings_filename, id_2_row_filename):        
+        embeddings = np.load(embeddings_filename)
+        book_id_2_row = json.load(open(id_2_row_filename, mode="r"))
+        
+        self.row_2_book_id = {row: book for book, row in book_id_2_row.items()}
+
+        self.index = faiss.IndexFlatL2(len(embeddings[0]))
+        embeddings = np.array(embeddings)
+        self.index.add(embeddings)
+    
+    def search(self, embedding, k):
+        distances, rows = self.index.search(embedding.reshape((1, -1)), k)
+        return [distance for distance in distances.reshape((-1))], [self.row_2_book_id[row] for row in rows.reshape((-1))]
+    
+class Index_cosine_similarity:
+
+    def normalize(self, embedding):
+        s = sum(embedding ** 2)
+        return embedding / np.sqrt(s)
+    
+    def __init__(self, embeddings_filename, id_2_row_filename):        
+        embeddings = [self.normalize(embedding) for embedding in np.load(embeddings_filename)]
+        book_id_2_row = json.load(open(id_2_row_filename, mode="r"))
+        
+        self.row_2_book_id = {row: book for book, row in book_id_2_row.items()}
+
+        self.index = faiss.IndexFlatIP(len(embeddings[0]))
+        embeddings = np.array(embeddings)
+        self.index.add(embeddings)
+    
+    def search(self, embedding, k):
+        distances, rows = self.index.search(self.normalize(embedding.reshape((1, -1))), k)
+        return [distance for distance in distances.reshape((-1))], [self.row_2_book_id[row] for row in rows.reshape((-1))]
+    
